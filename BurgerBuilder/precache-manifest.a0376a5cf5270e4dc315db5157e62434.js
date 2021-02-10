eed94737bb11e54d88ca211236ee9377f948f51e
vamshikrishnayedire <krishna.vamshi224@gmail.com>
2021-02-09 19:11:31 -0500

    Adding burger website

---
+++ BurgerBuilder/precache-manifest.a0376a5cf5270e4dc315db5157e62434.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b070ea3966281bad5dfa19163c0857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f337602cf594fb8bb63",
+    "url": "/static/css/main.a68c9143.chunk.css"
+  },
+  {
+    "revision": "bd6000e7fa18bbe8d880",
+    "url": "/static/js/2.e29c553c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e29c553c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f337602cf594fb8bb63",
+    "url": "/static/js/main.41951b1a.chunk.js"
+  },
+  {
+    "revision": "7c32ca8f8e809d015da3",
+    "url": "/static/js/runtime-main.943f7be6.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "./static/media/burger-logo.b8503d26.png"
+  }
+]);
